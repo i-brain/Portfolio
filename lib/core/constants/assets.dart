53acacaa668e92362701f00b46a96eadffa50c79
Ibrahim Mammadov <manqanat1@gmail.com>
2024-02-24 04:46:20 +0400

    Asset classes created, images added

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -1,3 +1,23 @@
-class AppIcons {}
+class AppIcons {
+  AppIcons._();
+}
 
-class AppImages {}
+class AppImages {
+  AppImages._();
+
+  static const _basePath = 'assets/images';
+
+  static const googlePlayLogo = '$_basePath/google_play.png';
+  static const appStoreLogo = '$_basePath/app_store.png';
+  static const vipexCover = '$_basePath/vipex_cover.png';
+  static const yaxCover = '$_basePath/yax_cover.png';
+  static const yaxDriverCover = '$_basePath/yax_driver_cover.png';
+  static const movementCover = '$_basePath/movement_cover.png';
+  static const ceoEventsCover = '$_basePath/ceo_cover.png';
+  static const accessoriesCover = '$_basePath/accessories_cover.png';
+  static const vipexLogo = '$_basePath/vipex_logo.png';
+  static const yaxLogo = '$_basePath/yax_logo.png';
+  static const movementLogo = '$_basePath/movement_logo.png';
+  static const ceoEventsLogo = '$_basePath/ceo_logo.png';
+  static const accessoriesLogo = '$_basePath/accessories_logo.png';
+}
