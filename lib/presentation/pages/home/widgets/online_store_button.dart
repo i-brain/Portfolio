8885c198596b37e18dd4b312a24fd95dd124ffbc
Ibrahim Mammadov <manqanat1@gmail.com>
2024-03-06 23:54:55 +0400

    OnlineStoreButton created, hover and open url funcinalities added

---
+++ lib/presentation/pages/home/widgets/online_store_button.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import '../../../../core/theme/app_text_styles.dart';
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:js' as js;
+
+class OnlineStoreButton extends StatefulWidget {
+  const OnlineStoreButton({
+    super.key,
+    required this.title,
+    required this.imageAsset,
+    required this.url,
+  });
+
+  final String title;
+  final String imageAsset;
+  final String url;
+
+  @override
+  State<OnlineStoreButton> createState() => _OnlineStoreButtonState();
+}
+
+class _OnlineStoreButtonState extends State<OnlineStoreButton> {
+  bool isHovering = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (event) {
+        setState(() {
+          isHovering = true;
+        });
+      },
+      onExit: (event) {
+        setState(() {
+          isHovering = false;
+        });
+      },
+      cursor: SystemMouseCursors.click,
+      child: InkWell(
+        onTap: () => _launcUrl(),
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 100),
+          padding: const EdgeInsets.all(4),
+          decoration: BoxDecoration(
+            color: isHovering ? Colors.black12 : null,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Column(
+            children: [
+              Image.asset(
+                widget.imageAsset,
+                height: 100,
+                width: 100,
+              ),
+              const SizedBox(height: 10),
+              Text(
+                widget.title,
+                style: AppTextStyles.styleW600.copyWith(
+                  fontSize: 16,
+                  color: Colors.blue,
+                ),
+                maxLines: 1,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _launcUrl() {
+    js.context.callMethod('open', [widget.url]);
+  }
+}
