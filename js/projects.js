a6c60bff8b8d360105b3aba3a3c93b9e3751b1e4
Ibrahim Mammadov <manqanat1@gmail.com>
2024-08-10 15:03:16 +0400

    Demo version

---
+++ js/projects.js
@@ -0,0 +1,17 @@
+const projects = [
+    {
+        coverImage: "images/project/yax_cover.png",
+        logoImage: "images/project/yax_logo.png",
+        title: "Yaxpoint",
+        googlePlayLink: "#", // Add the actual link here
+        appStoreLink: "#",   // Add the actual link here
+        description: "Yaxpoint is a dynamic transportation platform with separate apps for clients and drivers. Similar to Uber and Bolt, it offers a streamlined experience with real-time features and an intuitive design.",
+        features: [
+            "Integrated Google Maps for precise location search, efficient navigation, and optimized route planning.",
+            "In-app chat and call features that enable direct communication between clients and drivers.",
+            "Real-time ride tracking, automatic fare calculation, and secure payment processing to enhance user convenience."
+        ]
+    },
+    // Add more project objects here
+];
+
